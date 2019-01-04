2e0c65afa042c8a5a8f8a626992161a2640884c3
Leon Yan <nyxlingren@gmail.com>
2019-01-04 21:13:20 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-                        theme: ThemeData(
+      theme: ThemeData(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -37,7 +37,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+                final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
